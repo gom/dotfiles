c7356b048679499169ad75d7e2717d4a5a413602
gom <gomgom68@gmail.com>
2016-02-18 19:12:41 +0900

    Fix pythonz

--- setup/python.sh
+++ setup/python.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/zsh
-curl -kL https://raw.github.com/saghul/pythonz/master/pythonz-install | bash
-source $HOME/.zshenv && rehash
 
+# pythonz
+curl -kL https://raw.github.com/saghul/pythonz/master/pythonz-install | bash
+source $HOME/.zshrc && rehash
+
+# pip
+[ ! -e "`which pip`" ] && curl -kL https://bootstrap.pypa.io/get-pip.py | python
+sudo pip install virtualenv 
