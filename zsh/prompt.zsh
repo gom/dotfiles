065d52c97bfa7f33672f958fdd1734a09b6a871c
gom <gomgom68@gmail.com>
2016-10-25 08:55:57 +0900

    Fix for /usr/bin/log on Darwin

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -30,3 +30,21 @@ show_virtual_env() {
 }
 
 PS1='$(show_virtual_env)'$PS1
+
+# log
+watch="all"    # watching login and logout
+
+case ${OSTYPE} in
+  linux*)
+    # Darwin has /usr/bin/log.
+    log # show watching log
+    ;;
+esac
+
+setopt no_tify # when bg job finish, notify me.
+setopt nomatch
+setopt ignore_eof # ignore logout when touch ^D
+
+# job
+setopt long_list_jobs
+REPORTTIME=3
