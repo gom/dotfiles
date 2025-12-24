8b481ef458c3d3d76b278763feeaeff12dada2dc
gom <gomgom68@gmail.com>
2025-12-24 16:41:41 +0900

    feat: Autoload functions in zsh/functions
    - Implement autoloading for functions in the 'zsh/functions' directory
      by adding the directory to 'fpath' and using 'autoload'.
    - Rename 'git_bulk_pull.zsh' to 'bulk_pull.zsh' to match the function
      name for autoloading to work correctly.

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -1,3 +1,6 @@
+# Add the functions directory to fpath and autoload all functions
+fpath=(${_ZDOTDIR}/functions ${fpath})
+autoload -U ${_ZDOTDIR}/functions/*(:t)
 
 ## ssh-agent
 [ -e ${HOME}/.ssh/agent-env ] && source ${HOME}/.ssh/agent-env
