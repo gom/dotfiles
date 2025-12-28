5bee7a688113cdd9afde6f5990f39b6d52af3845
gom <gomgom68@gmail.com>
2025-12-28 21:09:58 +0900

    refactor: migrate zsh/functions logic to sheldon using multiline string

--- zsh/functions.zsh
+++
@@ -1,6 +0,0 @@
-# Add the functions directory to fpath and autoload all functions
-fpath=(${_ZDOTDIR}/functions ${fpath})
-autoload -Uz ${_ZDOTDIR}/functions/*(N:t)
-
-## ssh-agent
-[ -e ${HOME}/.ssh/agent-env ] && source ${HOME}/.ssh/agent-env
