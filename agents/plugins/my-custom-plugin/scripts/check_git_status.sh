91ae33ca0c5382226faa984e238b349652c9cfc9
gom <gomgom68@gmail.com>
2026-06-15 17:24:19 +0900

    Refactor: migrate agent configs to native chezmoi symlinks and remove python compiler

--- agents/plugins/my-custom-plugin/scripts/check_git_status.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Hook Name: check_git_status
-# Event: pre-command
-
-echo "🔄 Running Pre-Command Hook: Checking Git Status..."
-git status -s
--- agents/plugins/my-custom-plugin/scripts/git_helper.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Skill Name: git-helper
-# Description: Custom git helper script.
-
-echo "Git Helper skill triggered!"
-git status -s
